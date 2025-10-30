9128eba8fbff27869e79dc7c9b9be601cbb9d19d
bastient6 <bastien.tiffy@epitech.eu>
2025-10-30 15:28:37 +0100

    [UPDATE]_router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import './App.css'
+import { HashRouter, Routes, Route } from 'react-router-dom';import './App.css'
 import Home from "./pages/home"
 import Contact from "./pages/contact"
 import Nav from './contenaire/nav'
@@ -9,14 +8,14 @@ import { ScrollProvider } from "./context/ScrollContext";
 function App() {
 	return (
 		<ScrollProvider>
-			<Router>
+			<HashRouter>
 				<Nav/>
 					<Routes>
 						<Route path="/" element={<Home/>}/>
 						<Route path="/contact" element={<Contact/>}/>
 					</Routes>
 				<Footer/>
-			</Router>
+			</HashRouter>
 		</ScrollProvider>
 	);
 }
