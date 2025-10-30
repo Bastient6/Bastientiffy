fce7a1fa7446725b98d175991537afc8e92e0fda
bastient6 <bastien.tiffy@epitech.eu>
2025-10-30 15:44:22 +0100

    [UPDATE]_STATUS SCROLL

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,8 +1,10 @@
 import Star from "../contenaire/Star";
 import ContactCard from "../contenaire/contactCard.jsx";
+import { useScrollStatus } from "../context/ScrollContext";
 
 export default function Contact() {
-
+    const { setStatusScroll } = useScrollStatus();
+    setStatusScroll(false);
     return (
     <div className="flex justify-center items-center overflow-hidden h-screen bg-[#dae1e6]">
 		<div id="box" className="relative bg-[#00010E] rounded-4xl overflow-hidden w-4/5 h-3/4 z-1">
