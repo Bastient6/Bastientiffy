8b65c1a76a38239f2a9f69e1aa055aff3142194d
bastient6 <bastien.tiffy@epitech.eu>
2025-10-30 16:18:03 +0100

    [UPDATE]_meta

--- src/contenaire/apropos.jsx
+++ src/contenaire/apropos.jsx
@@ -4,7 +4,7 @@ export function Apropos() {
         <div id="apropos"className="fixed top-110 w-full flex flex-col items-center">
 			<h1 className="absolute text-center md:right-0 transform md:-translate-x-1/2 text-4xl text-[#ff7699] font-black text-5xl">À propos de moi</h1>
 			<div className="md:flex top-30 font-montserrat absolute text-left text-white text-2xl justify-center">
-				<img src="/photo.jpg" className="rounded-full w-1/5 ml-10 md:ml-0 md:w-1/4 h-1/4 mb-4 object-contain"/>
+				<img src="/photo.jpg" className="rounded-full w-1/5 ml-10 md:ml-0 md:w-1/4 h-1/4 mb-4 object-contain" alt="pp bastien"/>
 				<div className="flex flex-col md:w-1/4">
 					<p className="ml-10 leading-relaxed text-lg">Passionné par la tech,le developpement et l'automatisation, je me forme à Epitech Montpellier. Volontaire au SDIS 34, je combine rigueur, adaptabilité et esprit d'équipe au quotidien.</p>
 					<p className="mt-10 ml-10 leading-relaxed">Contact</p>
