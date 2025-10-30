4da6b4f6e5faadfe1a9769898e91f82bec8c86c0
bastient6 <bastien.tiffy@epitech.eu>
2025-10-30 15:35:22 +0100

    [UPDATE]_href to to

--- src/contenaire/nav.jsx
+++ src/contenaire/nav.jsx
@@ -5,7 +5,7 @@ export default function Nav() {
 
   	return (
     	<nav className={`flex items-center justify-between fixed top-0 left-0 w-full p-4 z-50 transition-all duration-300 ${statusScroll ? "text-white bg-transparent" : "text-[#00010e] bg-[#dae1e6]"}`}>
-        	<a href="/">
+        	<a to="/">
             	<img className="w-20 h-auto mr-4 rounded-md" src="/logo.png" alt="bastien" />
        		</a>
       		<div className={`absolute ${!statusScroll ? "left-1/2 transform -translate-x-1/2 text-2xl": "left-30"} text-2xl hidden sm:block`}>
@@ -13,7 +13,7 @@ export default function Nav() {
         		<h2>Développeur full-stack</h2>
       		</div>
       		<div className="relative z-10 ml-4 relative px-6 py-3 text-[#00010e] text-center bg-[#ff7699] rounded-md">
-          		<a href="/contact" >Contact</a>
+          		<a to="/contact" >Contact</a>
         	</div>
     	</nav>
   	);
