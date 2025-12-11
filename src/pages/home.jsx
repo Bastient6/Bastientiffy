8414641daa702cc8334d0c49f19874b3c5d889d1
Bastient6 <bastien.tiffy@epitech.eu>
2025-12-11 14:25:14 +0100

    [UPDATE]_scroll

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -50,43 +50,27 @@ export default function Home() {
 	}, [pos, status, setStatusScroll]);
 
 	return (
-		<div className="flex justify-center items-center min-h-screen
-						bg-[#dae1e6] md:bg-[#dae1e6] sm:bg-transparent">
-			<div
-			id="box"
-			onScroll={handleScroll}
-			className="relative bg-[#00010E] rounded-4xl overflow-y-scroll overflow-x-hidden
-						w-4/5 h-3/4 sm:w-full sm:h-full"
-			>
-			<div
-				className="stars absolute top-0 left-0 w-full h-full z-0 pointer-events-none"
-				style={{ transform: `translateY(${scrollPosition}px)` }}
-			>
-				<Star />
+			<div id="box" onScroll={handleScroll} className="relative bg-[#00010E] rounded-4xl overflow-y-scroll overflow-x-hidden w-4/5 h-3/4">
+				<div className="stars absolute top-0 left-0 w-full h-full z-0 pointer-events-none" style={{ transform: `translateY(${scrollPosition}px)` }}>
+					<Star />
+				</div>
+				<div className="flex flex-col items-center absolute top-3/7 w-full text-xl text-[#ff7699] p-4 text-center" style={{ transform: `translateY(${translateY * 0.1}px)`, transition: "transform"}}>
+					<TypeAnimation
+						sequence={[
+							"Étudiant en MSc à Epitech...",
+							1500,
+							"Développeur Web & Logiciel...",
+							1500,
+							"À la recherche d'une alternance !",
+							2000,
+						]}
+						repeat={Infinity}
+						className="text-4xl text-[#ff7699] text-center"
+					/>
+					<Apropos/>
+					<Projet/>
+					<Technique/>
+				</div>
 			</div>
-
-			<div
-				className="flex flex-col items-center absolute top-0 w-full text-xl text-[#ff7699] p-4 text-center"
-				style={{ transform: `translateY(${translateY * 0.1}px)`, transition: "transform 0.3s" }}
-			>
-				<TypeAnimation
-				sequence={[
-					"Étudiant en MSc à Epitech...",
-					1500,
-					"Développeur Web & Logiciel...",
-					1500,
-					"À la recherche d'une alternance !",
-					2000,
-				]}
-				repeat={Infinity}
-				className="text-4xl text-[#ff7699] text-center"
-				/>
-				<Apropos />
-				<Projet />
-				<Technique />
-			</div>
-			</div>
-		</div>
 	);
-
 }
